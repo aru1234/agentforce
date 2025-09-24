bf285479e4d5b5191034b9257d188cb32008553e
Amit Ukalkar <ukalkar.amit@ace.aaa.com>
2025-09-24 07:57:21 +0530

    working insurance faq lwc, classes and lightning types

---
+++ force-app/main/default/lwc/insuranceFAQs/insuranceFAQs.js
@@ -0,0 +1,32 @@
+import { LightningElement,api } from 'lwc';
+
+export default class InsuranceFAQs extends LightningElement {
+
+    text;
+    // source;
+
+    @api
+    get value() {
+        return this._value;
+    }
+    /**
+    * @param  {} value
+    */
+    set value(value) {
+        this._value = value;
+    }
+
+    connectedCallback(){
+        console.log('Connected Callback called.');
+        if(this.value){
+            console.log('value exists '+this.value);
+            console.log('parsed value '+JSON.stringify(this.value));
+            const parsedValue = this.value;
+            this.text = parsedValue.ResAnswer[0].text;
+             this.source = parsedValue.ResAnswer[0].source[0].source_Id;
+            console.log('text '+this.text); 
+            console.log('source '+this.source);
+
+        }
+    }
+}
